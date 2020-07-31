e7640d57c67b070f51e94e49ead7f414906782dc
XieJing520 <68905835+XieJing520@users.noreply.github.com>
2020-08-01 05:44:13 +0800

    Update CountCharactersWordsAndLinesInAFileQuestion13.java

--- chapter12_exercise/CountCharactersWordsAndLinesInAFileQuestion13.java
+++ chapter12_exercise/CountCharactersWordsAndLinesInAFileQuestion13.java
@@ -5,32 +5,34 @@ import java.util.*;
 
 public class CountCharactersWordsAndLinesInAFileQuestion13 {
 	public static void main(String[] args) throws Exception {
-		
-		File file = new File(args[0]);
-		
-		
-		if(!file.exists()) {
-			System.out.println(args[0]+"doesn't exist!!!");
-			System.exit(1);
+
+		File txtFile = new File("C:\\Users\\Administrator\\Desktop\\word.txt");
+
+		Scanner input = new Scanner(txtFile);
+
+		int chars, words, lines;
+
+		chars = words = lines = 0;
+
+		while (input.hasNext()) {
+			String string = input.nextLine();
+
+			String[] strings = string.split("[,.! \n]");
+
+			for (int i = 0; i < strings.length; i++) {
+				words++;
+				chars += strings[i].length();
+			}
+
+			lines++;
+
 		}
-		
-		Scanner input = new Scanner(file);
-		while(input.hasNext()) {
-			
-		}
-		
-		input = new Scanner(file);
-		while(input.hasNext()) {
-			
-		}
-		
-		input = new Scanner(file);
-		while(input.hasNext()) {
-			
-		}
-		
+
+		System.out.println("chars:" + chars);
+		System.out.println("words:" + words);
+		System.out.println("lines:" + lines);
+
+		input.close();
 
 	}
-
-	
 }
